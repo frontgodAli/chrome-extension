ae3ead08692378e032c28387a7bd8447c585fb3b
Ali hassi <alihassiperso@gmail.com>
2024-01-03 01:19:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
 const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
